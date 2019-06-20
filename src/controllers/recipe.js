d487e1bb842ec9865849328c102f7f7954007d10
Andre Weinkoetz <andre.weinkoetz@tum.de>
2019-06-20 18:24:16 +0200

    fixed #19

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -2,10 +2,12 @@ const lodash = require( 'lodash' );
 const RecipeModel = require( '../models/recipe' );
 
 const getAllRecipes = ( req, res ) => RecipeModel
-    .find( {} ).populate( 'ingredients' ).then( recipes => res.status( 200 ).json( recipes ) );
+    .find( {} ).then( recipes => res.status( 200 ).json( recipes ) );
 
 const getRecipeById = ( req, res ) => RecipeModel
-    .findById( { _id: req.params.id } ).populate( 'ingredients' ).then( recipe => res.status( 200 ).json( recipe ) );
+    .findById( { _id: req.params.id } )
+    .populate( { path: 'ingredients.ingredient' } )
+    .then( recipe => res.status( 200 ).json( recipe ) );
 
 const insertRecipe = ( req, res ) => {
     const newRecipe = lodash.cloneDeep( req.body.recipe );
