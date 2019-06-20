6ed1ce5ac6759db1fefa013252241b14fe0db94e
Andre Weinkoetz <andre.weinkoetz@tum.de>
2019-06-20 12:04:13 +0200

    implemented #16

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -4,6 +4,9 @@ const RecipeModel = require( '../models/recipe' );
 const getAllRecipes = ( req, res ) => RecipeModel
     .find( {} ).populate( 'ingredients' ).then( recipes => res.status( 200 ).json( recipes ) );
 
+const getRecipeById = ( req, res ) => RecipeModel
+    .findById( { _id: req.params.id } ).populate( 'ingredients' ).then( recipe => res.status( 200 ).json( recipe ) );
+
 const insertRecipe = ( req, res ) => {
     const newRecipe = lodash.cloneDeep( req.body.recipe );
     RecipeModel.create( newRecipe ).then( recipe => res.status( 200 ).json( recipe ) );
@@ -11,5 +14,6 @@ const insertRecipe = ( req, res ) => {
 
 module.exports = {
     getAllRecipes,
+    getRecipeById,
     insertRecipe,
 };
--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const RecipesController = require( '../controllers/recipe' );
 
 router.get( '/', RecipesController.getAllRecipes );
+router.get( '/:id', RecipesController.getRecipeById );
 router.post( '/', RecipesController.insertRecipe );
 
 module.exports = router;
