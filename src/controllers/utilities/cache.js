07e47702342ab0a0f253d636367583061f9da656
Andre Weinkoetz <andre.weinkoetz@tum.de>
2019-06-28 17:53:31 +0200

    added logging to cache #17

--- src/controllers/utilities/cache.js
+++ src/controllers/utilities/cache.js
@@ -2,14 +2,25 @@ const GoalModel = require( '../../models/goal' );
 const LifestyleModel = require( '../../models/lifestyle' );
 const AllergyModel = require( '../../models/allergy' );
 const CategoryModel = require( '../../models/category' );
+const logger = require( '../../logger' ).getLogger( 'CACHE' );
 
 const CACHE = {};
 
 const initCache = async () => {
+    logger.debug( 'Initializing Cache...' );
     CACHE.GOALS = await GoalModel.find().exec();
     CACHE.ALLERGIES = await AllergyModel.find().exec();
     CACHE.CATEGORIES = await CategoryModel.find().exec();
     CACHE.LIFESTYLES = await LifestyleModel.find().exec();
+    logger.debug( 'Cache initialized!' );
+    logger.debug( '### GOALS ### ' );
+    logger.debug( CACHE.GOALS );
+    logger.debug( '### ALLERGIES ### ' );
+    logger.debug( CACHE.ALLERGIES );
+    logger.debug( '### CATEGORIES ### ' );
+    logger.debug( CACHE.CATEGORIES );
+    logger.debug( '### LIFESTYLES ### ' );
+    logger.debug( CACHE.LIFESTYLES );
 };
 
 const getGoals = ( req, res ) => res.status( 200 ).json( CACHE.GOALS );
