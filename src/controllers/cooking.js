63c6ff16fe4907146d95e277ca2cf97b023c2fcc
Andre Weinkoetz <andre.weinkoetz@tum.de>
2019-07-10 12:23:26 +0200

    adding block for subs

--- src/controllers/cooking.js
+++ src/controllers/cooking.js
@@ -214,7 +214,7 @@ const blockSubstitution = async ( req, res ) => {
             .blockedSubstitutions
             .push( { orig: cookingEvent.possibleSubstitution.original, blockedSubs: [] } );
 
-        cookingEvent.save();
+        cookingEvent.persRecipe.save();
     }
 
     return res.status( 200 ).json( { msg: 'Success!' } );
