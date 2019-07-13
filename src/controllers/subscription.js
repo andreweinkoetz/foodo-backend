6b10b9ade8f8bac5c88ab1506fa451a6e3002884
Andre Weinkoetz <andre.weinkoetz@tum.de>
2019-07-13 19:57:47 +0200

    integrating paypal and subscription levels

--- src/controllers/subscription.js
+++ src/controllers/subscription.js
@@ -1,20 +1,49 @@
 const logger = require( '../logger' ).getLogger( 'SubscriptionController' );
-// const SubscriptionModel = require( '../models/subscription' );
-// const UserModel = require( '../models/user' );
+const SubscriptionModel = require( '../models/subscription' );
+const UserModel = require( '../models/user' );
 
 const createSubscription = ( req, res ) => {
-    logger.silly( 'Body' );
-    logger.silly( JSON.stringify( req.body ) );
+    logger.silly( 'Subscription create process started.' );
+
+    const { plan_id: planId, id } = req.body.resource;
+
+    SubscriptionModel.create( {
+        paypalSubscriptionId: id,
+        paypalPlanId: planId,
+        active: true,
+    } );
+
     res.status( 200 ).json( { msg: 'nice' } );
 };
 
-const cancelSubscription = ( req, res ) => {
-    logger.silly( 'Body' );
-    logger.silly( JSON.stringify( req.body ) );
-    res.status( 200 ).json( { msg: 'nice' } );
+const subscribeUser = async ( req, res ) => {
+    logger.silly( 'Subscription add user process started.' );
+    const { subscriptionId, userId } = req.body;
+    const subscription = await SubscriptionModel
+        .find( { paypalSubscriptionId: subscriptionId, active: true } ).exec();
+
+    if ( !subscription ) {
+        return res.sendStatus( 404 );
+    }
+
+    subscription.user = userId;
+    subscription.save();
+
+    UserModel.findOneAndUpdate( { _id: userId }, { level: 'subscribed' } ).exec();
+    return res.sendStatus( 200 );
+};
+
+const cancelSubscription = async ( req, res ) => {
+    logger.silly( 'Subscription cancel process started.' );
+    const paypalSubscriptionId = req.body.resource.id;
+    const subscription = await SubscriptionModel
+        .findOneAndRemove( { paypalSubscriptionId } ).exec();
+    UserModel.findOneAndUpdate( { _id: subscription.user }, { level: 'free' } ).exec();
+    res.sendStatus( 200 );
 };
 
 module.exports = {
     createSubscription,
     cancelSubscription,
+    subscribeUser,
 };
--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -1,11 +1,12 @@
 const express = require( 'express' );
 
 const router = express.Router();
-// const { checkAuthentication } = require( '../middlewares' );
+const { checkAuthentication } = require( '../middlewares' );
 
 const SubscriptionController = require( '../controllers/subscription' );
 
 router.post( '/create', SubscriptionController.createSubscription );
 router.post( '/cancel', SubscriptionController.cancelSubscription );
+router.put( '/subscribe', checkAuthentication, SubscriptionController.subscribeUser );
 
 module.exports = router;
